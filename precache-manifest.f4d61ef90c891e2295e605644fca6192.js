3e1b7928778c8608473a0003be076fdf6379efa4
Rohan Deshpande <rohandeshpande832@gmail.com>
2022-01-04 21:54:10 -0500

    Updates

---
+++ precache-manifest.f4d61ef90c891e2295e605644fca6192.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5370ebb9b4e2f907f2db561bce2eab8d",
+    "url": "/QBHacks/index.html"
+  },
+  {
+    "revision": "76222a42b8577f24c3ec",
+    "url": "/QBHacks/static/css/2.5c66aa0f.chunk.css"
+  },
+  {
+    "revision": "be36a2ea5ca4ede4645f",
+    "url": "/QBHacks/static/css/main.ce2a1c27.chunk.css"
+  },
+  {
+    "revision": "76222a42b8577f24c3ec",
+    "url": "/QBHacks/static/js/2.05cd6360.chunk.js"
+  },
+  {
+    "revision": "be36a2ea5ca4ede4645f",
+    "url": "/QBHacks/static/js/main.5b67ef0d.chunk.js"
+  },
+  {
+    "revision": "4ee078b35e5bf44e09f0",
+    "url": "/QBHacks/static/js/runtime~main.d28d9f3c.js"
+  },
+  {
+    "revision": "9fb616d601433fd59b2d33b1ef832505",
+    "url": "/QBHacks/static/media/QBLC.9fb616d6.jpg"
+  },
+  {
+    "revision": "9965263fe33deee423994d065691c000",
+    "url": "/QBHacks/static/media/aarav-small.9965263f.png"
+  },
+  {
+    "revision": "c8c60eb79dfb0e1850b82e9768ff4b93",
+    "url": "/QBHacks/static/media/aops.c8c60eb7.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/QBHacks/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/QBHacks/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/QBHacks/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/QBHacks/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/QBHacks/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "7be87580b7c9f8838d2dd98af918b80d",
+    "url": "/QBHacks/static/media/cloudspoit.7be87580.svg"
+  },
+  {
+    "revision": "cb5733104a756a6be8b777f1e8c38a16",
+    "url": "/QBHacks/static/media/creative.cb573310.svg"
+  },
+  {
+    "revision": "f3bc7b5d861fee9c980e404437dd472a",
+    "url": "/QBHacks/static/media/daniel-small.f3bc7b5d.png"
+  },
+  {
+    "revision": "fc38bd5fe862f3903a4be0a74e7c2f3f",
+    "url": "/QBHacks/static/media/darshil-new.fc38bd5f.jpg"
+  },
+  {
+    "revision": "78c9df0a633748c8ebe62fe63d842ea9",
+    "url": "/QBHacks/static/media/desmos.78c9df0a.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/QBHacks/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ee181b40a23050ea303e43064f73a506",
+    "url": "/QBHacks/static/media/hand.ee181b40.svg"
+  },
+  {
+    "revision": "c9a2c7d5ba78e013a3cfb908cc0a7e67",
+    "url": "/QBHacks/static/media/heart.c9a2c7d5.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/QBHacks/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/QBHacks/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/QBHacks/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/QBHacks/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/QBHacks/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cdb8d017f7f6643f8670e00a234c137a",
+    "url": "/QBHacks/static/media/jinal-small.cdb8d017.png"
+  },
+  {
+    "revision": "66498299ac362ee0727be6876deb6916",
+    "url": "/QBHacks/static/media/krish.66498299.jpg"
+  },
+  {
+    "revision": "18364f20fcec902a250859f08c50f75e",
+    "url": "/QBHacks/static/media/locahostmlh.18364f20.svg"
+  },
+  {
+    "revision": "ef880ddcc1597f81f5670504a1c6bd7d",
+    "url": "/QBHacks/static/media/logo1.ef880ddc.png"
+  },
+  {
+    "revision": "18356525a78979b357ae47cc697d5173",
+    "url": "/QBHacks/static/media/nabeel.18356525.jpg"
+  },
+  {
+    "revision": "1ea8b9467c034dec258c72c83edb3905",
+    "url": "/QBHacks/static/media/onepassword.1ea8b946.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/QBHacks/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/QBHacks/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/QBHacks/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/QBHacks/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/QBHacks/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "228dcc843a02f48ab0fdf876bf0cf75a",
+    "url": "/QBHacks/static/media/rohan-small.228dcc84.png"
+  },
+  {
+    "revision": "4571cbf26941cb3aa8c88e18119abb66",
+    "url": "/QBHacks/static/media/sashido.4571cbf2.png"
+  },
+  {
+    "revision": "7121ac56765d967d20b4982bdcb35a64",
+    "url": "/QBHacks/static/media/sketch.7121ac56.png"
+  },
+  {
+    "revision": "0d331a682097d47a1b39723d369d839c",
+    "url": "/QBHacks/static/media/ushack.0d331a68.png"
+  },
+  {
+    "revision": "7b08258be5923b3e952e057f6b634c4d",
+    "url": "/QBHacks/static/media/vent.7b08258b.png"
+  },
+  {
+    "revision": "2dddfc19a052825b416e4979bb4f7e61",
+    "url": "/QBHacks/static/media/wolf.2dddfc19.png"
+  },
+  {
+    "revision": "72e77b20435d91b8258b3fc81e2105f7",
+    "url": "/QBHacks/static/media/ziplyne.72e77b20.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.177b8f1219210cdb495c6618f84f5b2a.js"
+  "/QBHacks/precache-manifest.f4d61ef90c891e2295e605644fca6192.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QBHacks/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
